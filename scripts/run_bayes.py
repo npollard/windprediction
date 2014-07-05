0120e239bdfc3edd913e205635bd5e19d8012d6d
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-07-05 15:31:48 -0700

    Figured out regex in run_bayes

--- scripts/run_bayes.py
+++ scripts/run_bayes.py
@@ -1,19 +1,32 @@
+import re
 from os import walk
 from subprocess import call
 
-data = "data/KCASANTA132.nom.arff";
-models_path = "/Users/nelson/Documents/schools/independent_study/models/single_station"
+data_dir = "data"
+models_dir = "models"
+
 models = []
-for (dirpath, dirnames, filenames) in walk(models_path):
+for (dirpath, dirnames, filenames) in walk(models_dir):
   models.extend(filenames)
+  break
+
+datasets = []
+for (dirpath, dirnames, filenames) in walk(data_dir):
+  for filename in filenames: 
+    if re.match('K[A-Za-z]{7}[0-9]{1,3}.csv$', filename):
+      print filename
+      datasets.append(filename)
   break
 
 for model in models:
   print "________________________"
-  print "MODEL: " + model
+  print "MODEL: " + model + "\n"
 
-  command = "java weka.classifiers.bayes.BayesNet -t data/KCASANTA132.nom.arff -Q weka.classifiers.bayes.net.search.fixed.FromFile -- -B models/single_station/" + model + " -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 1.0"
-  print command + "\n\n"
-  call(command, shell=True)
+for data in datasets:
+  print "________________________"
+  print "DATA: " + data + "\n"
 
-  print "\n\n"
+#  command = "java weka.classifiers.bayes.BayesNet -t data/KCASANTA132.nom.arff -Q weka.classifiers.bayes.net.search.fixed.FromFile -- -B models/single_station/" + model + " -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 1.0"
+  #print command + "\n\n"
+  #call(command, shell=True)
+
