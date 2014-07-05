28217fec76880b2a10173e87f2edf1a17f5ab1cd
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-07-05 16:23:24 -0700

    run_bayes modification - increase heap size, etc.

--- scripts/run_bayes.py
+++ scripts/run_bayes.py
@@ -6,27 +6,33 @@ data_dir = "data"
 models_dir = "models"
 
 models = []
+print "MODELS:"
 for (dirpath, dirnames, filenames) in walk(models_dir):
-  models.extend(filenames)
+  for filename in filenames:
+    if re.match('.*\.xml$', filename):
+      print filename
+      models.append(filename)
   break
 
 datasets = []
+print "\nDATASETS:"
 for (dirpath, dirnames, filenames) in walk(data_dir):
   for filename in filenames: 
-    if re.match('K[A-Za-z]{7}[0-9]{1,3}.csv$', filename):
+    if re.match('K[A-Za-z]{7}[0-9]{1,3}\.nom\.arff$', filename):
       print filename
       datasets.append(filename)
   break
+print "\n"
 
 for model in models:
-  print "________________________"
-  print "MODEL: " + model + "\n"
+  print "___________________________________________"
+  print "MODEL: " + model
 
-for data in datasets:
-  print "________________________"
-  print "DATA: " + data + "\n"
+  for data in datasets:
+    print "\t_____________________________"
+    print "\tDATA: " + data
 
-#  command = "java weka.classifiers.bayes.BayesNet -t data/KCASANTA132.nom.arff -Q weka.classifiers.bayes.net.search.fixed.FromFile -- -B models/single_station/" + model + " -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 1.0"
-  #print command + "\n\n"
-  #call(command, shell=True)
+    command = "java -Xmx1500m weka.classifiers.bayes.BayesNet -v -o -t " + data_dir + "/" + data + " -Q weka.classifiers.bayes.net.search.fixed.FromFile -- -B " + models_dir + "/" + model + " -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 1.0"
+    print "\t" + command
+    call(command, shell=True)
 
