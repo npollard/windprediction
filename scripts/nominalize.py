064680b6e85eb8029d4070e49a2a7768f236c385
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-07-05 15:54:20 -0700

    Nominalize script "done"

--- scripts/nominalize.py
+++ scripts/nominalize.py
@@ -8,12 +8,9 @@ for (dirpath, dirnames, filenames) in walk(data_dir):
   for filename in filenames: 
     m = re.match('(K[A-Za-z]{7}[0-9]{1,3}).csv$', filename)
     if m:
-      print m.group()
-      print m.group(1) + ".nom.arff"
+      command = "java weka.filters.unsupervised.attribute.Discretize -i " + data_dir + "/" + m.group() + " -o " + data_dir + "/" + m.group(1) + ".nom.arff"
+      print command
+      call(command, shell=True)
 
   break
-
-#  command = "java weka.classifiers.bayes.BayesNet -t data/KCASANTA132.nom.arff -Q weka.classifiers.bayes.net.search.fixed.FromFile -- -B models/single_station/" + model + " -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 1.0"
-  #print command + "\n\n"
-  #call(command, shell=True)
 
